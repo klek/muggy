4ded99c5b810af491b5347d9e37bf645ab6feae3
Tobias Pettersson <a.tobias.pettersson@gmail.com>
2024-03-15 20:07:16 +0100

    REFACTOR: Move and add additional macros
    This commit adds a myMacros.h header-file in the common folder that is
    intended to be the home of general macros used in the engine. Existing
    macros have also been moved here

--- muggy/code/common/common.h
+++ muggy/code/common/common.h
@@ -11,25 +11,6 @@
 
 //#pragma warning(disables 4530) // Disable exception warning
 
-// Defining global macros
-#ifdef DEBUG_BUILD
-#define DEBUG_OP(x)             x
-#else
-#define DEBUG_OP(x)         
-#endif
-
-#define BIT(x)                  (1 << x)
-
-// Count of macro
-// #if ( defined(MSVC) )
-// #define COUNT_OF(arr)           _countof(arr)
-// #elif ( defined(GCC) )
-// #define BUILD_BUG_ON_ZERO(e)    ( sizeof( struct { int:-!!(e); } ) )
-// #define MUST_BE_ARRAY(arr)      BUILD_BUG_ON_ZERO( __builtin_types_compatible_p( typeof(a), typeof( &a[0] ) ) )
-// #define COUNT_OF(arr)           ( sizeof(arr) / sizeof((arr)[0]) + MUST_BE_ARRAY(arr) )
-// #endif
-
-
 // Common includes for the the engine
 // C/C++ 
 #include <cstdint>
@@ -42,6 +23,7 @@
 
 
 // Internal common headers
+#include "myMacros.h"
 #include "myTypes.h"
 #include "id.h"
 #include "../utilities/utilities.h"
---
+++ muggy/code/common/myMacros.h
@@ -0,0 +1,43 @@
+//********************************************************************
+//  File:    myMacros.h
+//  Date:    Thu, 14 Mar 2024: 21:26
+//  Version: 
+//  Author:  klek
+//  Notes:   
+//********************************************************************
+
+#if !defined(MY_MACROS_H)
+#define MY_MACROS_H
+
+// Defining global macros for debugging purposes
+#if defined( DEBUG_BUILD )
+#define DEBUG_OP(x)             x
+#define MSG(x)                  std::cout << x << std::endl;
+#else
+#define DEBUG_OP(x)             
+#define MSG(x)                  
+#endif
+
+#define BIT(x)                  (1 << x)
+
+// Class macros
+#if !defined ( MUGGY_DISABLE_COPY )
+#define MUGGY_DISABLE_COPY(T)                                       \
+                                explicit T( const T& ) = delete;    \
+                                T& operator=(const T&) = delete;
+#endif
+
+#if !defined ( MUGGY_DISABLE_MOVE )
+#define MUGGY_DISABLE_MOVE(T)                                       \
+                                explicit T( T&& ) = delete;         \
+                                T& operator=( T&&) = delete;
+#endif
+
+#if !defined ( MUGGY_DISABLE_COPY_AND_MOVE )
+#define MUGGY_DISABLE_COPY_AND_MOVE(T)                              \
+                                MUGGY_DISABLE_COPY(T)               \
+                                MUGGY_DISABLE_MOVE(T)
+#endif
+
+
+#endif
